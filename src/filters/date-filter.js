13426992e5024c75cd3875bfa0091796f03468d6
Bruno Pulis <brunopulis@protonmail.com>
2022-02-02 22:09:36 -0300

    fix: change the suffix

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -1,15 +1,8 @@
-// Stolen from https://stackoverflow.com/a/31615643
-const appendSuffix = n => {
-  var s = ['th', 'st', 'nd', 'rd'],
-    v = n % 100;
-  return n + (s[(v - 20) % 10] || s[v] || s[0]);
-};
-
 module.exports = function dateFilter(value) {
   const dateObject = new Date(value);
 
   const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
-  const dayWithSuffix = appendSuffix(dateObject.getDate());
+  const days = dateObject.getDate();
 
-  return `${dayWithSuffix} ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
+  return `${days} ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
 };
