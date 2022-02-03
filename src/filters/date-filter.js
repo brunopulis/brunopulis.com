6e1e9ee140864773c6531e14a89c45f2013bc93e
Bruno Pulis <brunopulis@protonmail.com>
2022-02-03 10:15:26 -0300

    fix: formatted date to pt-br

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -1,8 +1,14 @@
 module.exports = function dateFilter(value) {
+  const options = { year: "numeric", month: "long", day: "numeric" };
   const dateObject = new Date(value);
+  
+  dateObject.toLocaleDateString("pt-br", options);
+  
+  let formattedDate = dateObject.toLocaleDateString("pt-br", { ...options, month: 'numeric' });
 
   const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
   const days = dateObject.getDate();
 
-  return `${days} ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
+  // return `${days} ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
+  return formattedDate;
 };
