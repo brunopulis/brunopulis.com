3d37b17965fc1ae32f46d90683a106c52ea5f74a
Bruno Pulis <brunopulis@protonmail.com>
2024-06-20 19:16:54 -0300

    feat: add button styles

--- template-parts/home-blog.php
+++ template-parts/home-blog.php
@@ -1,48 +1,50 @@
-<section  class="recent-blog-posts mt-5" aria-labelledby="blog-title">
-  <div class="headline p-1">
+<section  class="section" aria-labelledby="blog-title">
     <div class="container">
       <div class="row">
-        <h2 id="letters-title">Últimos artigos</h2>
+        <div class="d-flex align-items-center justify-content-between">
+          <h2 id="letters-title">Últimas publicações</h2>
+          <a href="#" class="button">veja todas as publicações <span class="visually-hidden">do blog</span></a>
+        </div>
       </div>
     </div>
   </div>
 
   <div class="container">
     <div class="row">
-      <ul class="home-blog__list">
-        <?php
-          $args = array(
-            'post_type' => 'post',
-            'posts_per_page' => 6,
-            'order' => 'DESC'
-          );
+      <?php
+        $args = array(
+          'post_type' => 'post',
+          'posts_per_page' => 3,
+          'order' => 'DESC'
+        );
 
-          $blog = new WP_Query( $args );
+        $blog = new WP_Query( $args );
 
-          if ( $blog->have_posts() ) :
-            while ( $blog->have_posts() ) : $blog->the_post();
-        ?>
-          <li class="home-blog__item">
-            <article class="c-card c-card--transparent">
-              <div class="c-card__body">
-                <p class="post-date">
-                  <time class="time" datetime="<?php echo get_the_date('Y-m-d'); ?>">
-                    <?php echo get_the_date('j F, Y'); ?>
-                  </time>
-                </p>
-                <h3 class="c-card__title">
-                  <a href="<?php the_permalink(); ?>" class="c-card__title">
-                    <?php the_title(); ?>
-                  </a>
-                </h3>
+        if ( $blog->have_posts() ) :
+          while ( $blog->have_posts() ) : $blog->the_post();
+      ?>
+        <article class="home-blog__item col-lg-4">
+          <div class="c-card h-100">
+            <div class="c-card__body">
+              <h3 class="c-card__title">
+                <a href="<?php the_permalink(); ?>" class="c-card__link">
+                  <?php the_title(); ?>
+                </a>
+              </h3>
+              <time class="c-card__meta" datetime="<?php echo get_the_date('Y-m-d'); ?>">
+                <?php echo get_the_date('j F, Y'); ?>
+              </time>
+
+              <div class="c-card__content">
+                <p><?php the_excerpt(); ?></p>
+                <a href="#" class="c-card__link">Leia o texto <span class="visually-hidden">sobre: <?php the_title();?></span></a>
               </div>
-            </article>
-          </li>
-        <?php endwhile; ?>
-        <?php wp_reset_postdata(); ?>
-        <?php endif; ?>
-      </ul>
+            </div>
+          </div>
+        </article>
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); ?>
+      <?php endif; ?>
     </div>
-    <a href="<?php the_permalink(); ?>/blog" class="button button--wheat">Leia outras edições</a>
   </div>
 </section>
