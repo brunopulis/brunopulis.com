071a06633d6181669ada8c3ba9c05abb6e2ccd41
Bruno Pulis <brunopulis@protonmail.com>
2024-06-20 14:46:56 -0300

    adjustments in hero section

--- template-parts/home-hero.php
+++ template-parts/home-hero.php
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row d-flex align-items-center">
         <div class="col-xs-12 col-lg-6">
-          <h1 class="fw-bold">
+          <h1 class="c-hero__title">
             <span class="me" rel="me">Bruno Pulis</span>
           </h1>
           <p class="c-hero__lead">
@@ -15,8 +15,8 @@
           <a href="<?php bloginfo('url'); ?>/servicos" class="c-hero__link">Conheça meus serviços</a>
         </div>
 
-        <div class="col-xs-12 col-lg-6 d-none d-md-block">
-          <figure class="d-flex align-items-center justify-content-end">
+        <div class="col-xs-12 col-lg-6">
+          <figure class="c-hero__image">
             <img src="<?php echo TEMPLATE_PATH; ?>/assets/images/profile.png" loading="lazy" alt="">
           </figure>
         </div>
