51fed7a136d495cf2715edd52fe21161471ced3b
Bruno Pulis <brunopulis@protonmail.com>
2024-09-01 16:26:59 -0300

    fix: remove commentaries

--- comments.php
+++ comments.php
@@ -77,11 +77,6 @@
 			'class_submit' 			=> 'submit button'
 		));
 	?>
-	
-    <div class="reply-email">
-	    <h2>Responder por e-mail</h2>
-	    <p>Adoraria ouvir o que você achou. Mande um e-mail para <a href="mailto:hello@brunopulis.com?subject=Resposta%20ao%20artigo%20<?php the_title(); ?>"> hello@brunopulis.com</a>; não se esqueça de me informar se está satisfeito com o fato de seu comentário aparecer na Web!</p>
-	</div>
     </div>
   </div>
 </section><!-- #comments -->
