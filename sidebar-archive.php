551add4badc1a3fa9a691c0262a547ae5f02f3e7
Bruno Pulis <brunopulis@protonmail.com>
2024-09-08 02:24:36 -0300

    fixing itens

--- sidebar-archive.php
+++ sidebar-archive.php
@@ -12,7 +12,6 @@
 <aside class="sidebar" id="sidebar" role="complementary">
   <?php
 		if ( ! dynamic_sidebar( 'archive-sidebar' ) ) {
-			the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ) );
 			the_widget( 'WP_Widget_Archives', array( 'count' => 0, 'dropdown' => 1 ) );
 			the_widget( 'WP_Widget_Tag_Cloud' );
 		}
