7d9b739b8d499d340a0336e910385d2ba41ecf64
Bruno Pulis <brunopulis@protonmail.com>
2024-09-16 18:09:41 -0300

    feat: add bookmark components

--- archive-links.php
+++ archive-links.php
@@ -4,66 +4,67 @@
  *
  * The template for displaying Link page.
  *
- * @package Odin
+ * @package Bruno_Pulis
  * @since 2.2.0
  */
 get_header(); ?>
 
 <main id="content">
   <div class="container">
-    <div class="row">  
+    <div class="row mb-4">  
       <h1 class="entry-title">Links</h1>
       <?php odin_breadcrumbs(); ?>
       <p>Links interessantes que vi ao longo da semana.</p>
+    </div>
      
-      <div class="col-md-8">
-        <div class="row">
-          <?php
-            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-            $args = array(
-              'post_type'      => 'bookmark',
-              'paged' => $paged,
-              'posts_per_page' => 10,
-              'order'          => 'DESC'
-            );
+      <ul class="bookmark__list">
+        <?php
+          $args = array(
+            'post_type'      => 'bookmark',
+            'posts_per_page' => -1,
+            'order'          => 'DESC'
+          );
 
-            $bookmark = new WP_Query( $args );
+          $bookmark = new WP_Query( $args );
+          
+          if ( $bookmark->have_posts() ) :
+            while ( $bookmark->have_posts() ) : $bookmark->the_post();
+              $sindycateLink = get_field( 'bookmark_url' );
+        ?>
+          <li class="bookmark__item">
+            <h3 class="bookmark__title"><a href="<?php echo $sindycateLink; ?>" class="bookmark__link text-no-underline"><?php the_title(); ?></a></h3>
+            <p class="bookmark__text"><?php the_content(); ?></p>
             
-            if ( $bookmark->have_posts() ) :
-              while ( $bookmark->have_posts() ) : $bookmark->the_post();
-                $sindycateLink = get_field( 'bookmark_url' );
-          ?>
-            <article class="home-blog__item col-lg-4">
-              <div class="c-card h-100">
-                <div class="c-card__body">
-                  <h3 class="c-card__title">
-                    <a href="<?php the_permalink(); ?>" class="c-card__link">
-                      <?php the_title(); ?>
-                    </a>
-                  </h3>
-                  <time class="c-card__meta" datetime="<?php echo get_the_date('Y-m-d'); ?>">
-                    <?php echo get_the_date('j F, Y'); ?>
-                  </time>
-
-                  <div class="c-card__content">
-                    <p><?php the_content(); ?></p>
-                    <a href="<?php the_permalink(); ?>" class="c-card__link">Leia o texto <span class="visually-hidden">sobre: <?php the_title();?></span></a>
-                  </div>
-                </div>
-              </div>
-            </article>
-            <?php endwhile;?>
+            <footer class="bookmark__footer">
+              <time class="bookmark__date" datetime="<?php echo get_the_date('Y-m-d'); ?>">
+                <?php echo get_the_date('j F, Y'); ?>
+              </time>
+            </footer>
+          </li>
+          <?php endwhile;?>
             <div class="pagination">
-              <?php wp_pagenavi( array( 'query' => $bookmark ) ); ?>
+              <?php
+                echo paginate_links( 
+                  array( 
+                    'before_page_number'=> '<span class="visually-hidden" aria-hidden="true">',
+                    'after_page_number'=> '</span>',
+                    'next_text'    => 'Artigos antigos',
+                    'prev_text'    => 'Novos artigos'
+                  ) 
+                ); 
+              ?>
             </div>
             <?php wp_reset_postdata(); ?>
         <?php else: ?>
           <p>Nenhum link encontrado.</p>
         <?php endif; ?>  
+            </ul>
+           
         </div>
       </div>
     </div>
   </div>
+  <?php require_once('template-parts/newsletter.php'); ?>
 </main><!-- #main -->
 
 <?php
