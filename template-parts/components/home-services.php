85f720b8b842ce02c8f5abce0fc9a2fc2fd4c4bb
Bruno Pulis <brunopulis@protonmail.com>
2024-10-11 20:41:52 -0300

    feat: add services section

--- template-parts/components/home-services.php
+++ template-parts/components/home-services.php
@@ -1,46 +1,53 @@
-<section class="section section--services" id="services" aria-labelledby="services-title">
+<section  class="section" aria-labelledby="services-title">
   <div class="container">
     <div class="row">
       <div class="d-flex align-items-center justify-content-between">
         <h2 class="section-title" id="services-title">Meus serviços</h2>
-        <a href="http://localhost/brunopulis/blog" class="button">veja todas os serviços</a>
+        <a href="<?php bloginfo( 'url' ) ?>/servicos" class="button">veja todos os serviços</a>
       </div>
     </div>
+  </div>
 
+  <div class="container">
     <div class="row">
-      <?php
-        $args = array(
-          'post_type'      => 'services',
-          'posts_per_page' => 3,
-          'order'          => 'ASC'
-        );
-
-        $services = new WP_Query( $args );
-
-        if ( $services->have_posts() ) :
-          while ( $services->have_posts() ) : $services->the_post();
-          
-          $customClass = get_field('services_classes');
-      ?>
-        <div class="col-lg-4 mb-4">
-          <article class="c-card h-100">
-            <div class="service c-card__body">
-              <header class="service__header">
-                <svg aria-hidden="true" class="service__icon service__icon--<?php echo $customClass; ?>">
-                  <use xlink:href="<?php echo TEMPLATE_PATH; ?>/assets/images/svgs/solid.svg#<?php echo $customClass; ?>">
-                </use>
-                </svg>
-                <h3 class="c-card__title"><?php the_title(); ?></h3>
-              </header>
-              <div class="c-card__content">
-                <?php the_content(); ?>
-              </div>
+      <article class="home-blog__item col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h3 class="card-title">Desenvolvimento web</h3>
+            <div class="card-text mt-3">
+              <p>Criação de websites acessíveis, desenvolvidos com foco na usabilidade para todos os usuários, incluindo pessoas com deficiência, garantindo uma presença online inclusiva.</p>
             </div>
-          </article>
+
+            <a href="<?php the_permalink(); ?>" class="card-link">Leia o texto <span class="visually-hidden">sobre: <?php the_title();?></span></a>
+          </div>
         </div>
-      <?php endwhile; ?>
-      <?php wp_reset_postdata(); ?>
-      <?php endif; ?>
+      </article>
+
+      <article class="home-blog__item col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h3 class="card-title">Consultorias e Mentorias</h3>
+            <div class="card-text mt-3">
+              <p>Orientação especializada para implementar políticas, práticas e padrões de acessibilidade, garantindo que seu negócio esteja alinhado com as normas e boas práticas.</p>
+            </div>
+
+            <a href="<?php the_permalink(); ?>" class="card-link">Leia o texto <span class="visually-hidden">sobre: <?php the_title();?></span></a>
+          </div>
+        </div>
+      </article>
+
+      <article class="home-blog__item col-lg-4">
+        <div class="card">
+          <div class="card-body">
+            <h3 class="card-title">Palestras</h3>
+            <div class="card-text mt-3">
+              <p>Palestras inspiradoras e educativas sobre a importância da acessibilidade, sensibilizando seu público para a inclusão e o impacto positivo de práticas acessíveis..</p>
+            </div>
+
+            <a href="<?php the_permalink(); ?>" class="card-link">Leia o texto <span class="visually-hidden">sobre: <?php the_title();?></span></a>
+          </div>
+        </div>
+      </article>
     </div>
   </div>
 </section>
