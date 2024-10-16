0c34ed3d26e913d5e4b6b975d2373660ee75597e
Bruno Pulis <brunopulis@protonmail.com>
2024-10-16 18:16:30 -0300

    fix: content

--- template-parts/components/home-services.php
+++ template-parts/components/home-services.php
@@ -1,36 +1,36 @@
-<section  class="section" aria-labelledby="services-title">
-  <div class="container">
-    <div class="row">
-      <div class="d-flex align-items-center justify-content-between">
+<section class="section" aria-labelledby="services-title">
+  <header class="section__header" role="banner">
+    <div class="container">
+      <div class="section__wrap">
         <h2 class="section-title" id="services-title">Meus serviços</h2>
-        <a href="<?php bloginfo( 'url' ) ?>/servicos" class="button">veja todos os serviços</a>
+        <a href="<?php bloginfo( 'url' ) ?>/servicos" class="button">todos os serviços</a>
       </div>
     </div>
-  </div>
+  </header>
 
-  <div class="container">
-    <div class="row">
-      <article class="home-blog__item col-lg-4">
-        <div class="card">
+  <div class="container mt-5">
+    <ul class="list-grid">
+      <li>
+        <article class="card h-100">
           <div class="card-body">
-
             <h3 class="card-title">
               <svg aria-hidden="true" class="service__icon" width="24" height="24">
                 <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/svgs/solid.svg#code"></use>
               </svg>
-              Desenvolvimento web
+              Desenvolvimento de sites
             </h3>
             <div class="card-text mt-3">
-              <p>Criação de websites acessíveis, desenvolvidos com foco na usabilidade para todos os usuários, incluindo pessoas com deficiência, garantindo uma presença online inclusiva.</p>
+              <p>
+                Criação de websites, landing pages e customização de temas WordPress. Com foco em usabilidade e acessibilidade.</p>
             </div>
-
-            <a href="<?php the_permalink(); ?>" class="card-link">Leia o texto <span class="visually-hidden">sobre: <?php the_title();?></span></a>
+            <!--<a href="<?php the_permalink(); ?>" class="button">
+              fazer orçamento <span class="visually-hidden">de desenvolvimento</span>
+            </a>-->
           </div>
-        </div>
-      </article>
-
-      <article class="home-blog__item col-lg-4">
-        <div class="card">
+        </article>
+      </li>
+      <li>
+        <article class="card h-100">
           <div class="card-body">
             <h3 class="card-title">
               <svg aria-hidden="true" class="service__icon" width="24" height="24">
@@ -41,14 +41,14 @@
             <div class="card-text mt-3">
               <p>Orientação especializada para implementar políticas, práticas e padrões de acessibilidade, garantindo que seu negócio esteja alinhado com as normas e boas práticas.</p>
             </div>
-
-            <a href="<?php the_permalink(); ?>" class="card-link">Leia o texto <span class="visually-hidden">sobre: <?php the_title();?></span></a>
+            <!--<a href="<?php the_permalink(); ?>" class="button">
+              fazer orçamento <span class="visually-hidden">de desenvolvimento</span>
+            </a>-->
           </div>
-        </div>
-      </article>
-
-      <article class="home-blog__item col-lg-4">
-        <div class="card">
+        </article>
+      </li>
+      <li>
+        <article class="card h-100">
           <div class="card-body">
             <h3 class="card-title">
               <svg aria-hidden="true" class="service__icon" width="24" height="24">
@@ -59,11 +59,73 @@
             <div class="card-text mt-3">
               <p>Palestras inspiradoras e educativas sobre a importância da acessibilidade, sensibilizando seu público para a inclusão e o impacto positivo de práticas acessíveis..</p>
             </div>
-
-            <a href="<?php the_permalink(); ?>" class="card-link">Leia o texto <span class="visually-hidden">sobre: <?php the_title();?></span></a>
+            <!--<a href="<?php the_permalink(); ?>" class="button">
+              fazer orçamento <span class="visually-hidden">de desenvolvimento</span>
+            </a>-->
           </div>
-        </div>
-      </article>
-    </div>
+        </article>
+      </li>
+      <li>
+        <article class="card h-100">
+          <div class="card-body">
+            <h3 class="card-title">
+              <svg aria-hidden="true" class="service__icon" width="24" height="24">
+                <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/svgs/brands.svg#accessible-icon"></use>
+              </svg>
+              Auditoria de acessibilidade
+            </h3>
+            <div class="card-text mt-3">
+              <p>
+                Produzimos um relatório detalhado apontado as falhas em relação às diretrizes de acessibilidade e outros padrões nacionais e internacionais.
+                E indicamos como corrigir.
+              </p>
+
+            </div>
+            <!--<a href="<?php the_permalink(); ?>" class="button">
+              fazer orçamento <span class="visually-hidden">de desenvolvimento</span>
+            </a>-->
+          </div>
+        </article>
+      </li>
+      <li>
+        <article class="card h-100">
+          <div class="card-body">
+            <h3 class="card-title">
+              <svg aria-hidden="true" class="service__icon" width="24" height="24">
+                <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/svgs/solid.svg#network-wired"></use>
+              </svg>
+              Remediação de acessibilidade
+            </h3>
+            <div class="card-text mt-3">
+              <p>
+                Não sabe ou não tem tempo para corrigir problemas de acessibilidade?
+                Essa solução é ideal para esse cenário. Avaliamos e corrigimos.
+              </p>
+            </div>
+            <!--<a href="<?php the_permalink(); ?>" class="button">
+              fazer orçamento <span class="visually-hidden">de desenvolvimento</span>
+            </a>-->
+          </div>
+        </article>
+      </li>
+      <li>
+        <article class="card h-100">
+          <div class="card-body">
+            <h3 class="card-title">
+              <svg aria-hidden="true" class="service__icon" width="24" height="24">
+                <use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/svgs/solid.svg#network-wired"></use>
+              </svg>
+              Documentos acessíveis
+            </h3>
+            <div class="card-text mt-3">
+              <p>Corrigimos problemas de acessibilidade em documentos como: PDF, Word, Excel e Powerpoint.</p>
+            </div>
+            <!--<a href="<?php the_permalink(); ?>" class="button">
+              fazer orçamento <span class="visually-hidden">de desenvolvimento</span>
+            </a>-->
+          </div>
+        </article>
+      </li>
+    </ul>
   </div>
 </section>
