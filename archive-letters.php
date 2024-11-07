eac555411c51923240738c5d87760f24ee55a466
Bruno Pulis <brunopulis@protonmail.com>
2024-11-07 15:41:40 -0300

    fix: pagination

--- archive-letters.php
+++ archive-letters.php
@@ -15,23 +15,35 @@
  * @package Bruno_Pulis
  * @since 2.2.0
  */
-
- // Todo: Finish style
-
 get_header(); ?>
 
-<main id="content">
+<main id="content"> 
+  <section class="hero">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6">
+          <?php odin_breadcrumbs(); ?>
+          <span class="tagline">Insights valiosos sobre acessibilidade, Obsidian e organização digital.</span>
+          <h1>Assine a Pulis Letters</h1>
+          <p>
+          Inscreva-se para se juntar a 500.000+ outros assinantes e receba e-mails regulares sobre neurociência, saúde e ferramentas relacionadas à ciência do Dr. Andrew Huberman.
+          </p>
+          <?php echo do_shortcode( '[wpforms id="1048"]' );  ?>
+        </div>
+      </div>
+    </div>
+  </section>
   <div class="container">
     <div class="row">
-      <h1>Pulis Letters</h1>
-      <p>Insights valiosos sobre acessibilidade, Obsidian e organização digital.</p>
-     
       <div class="col-md-8">
+        <h2>Edições anteriores</h2>
         <div class="row">
           <?php
+            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
             $args = array(
               'post_type'      => 'letters',
-              'posts_per_page' => 8,
+              'paged'          => $paged,
+              'posts_per_page' => 3,
               'order'          => 'DESC'
             );
 
@@ -41,8 +53,8 @@ get_header(); ?>
               while ( $letters->have_posts() ) : $letters->the_post();
                 $edition = get_field( 'letters_edition' );
           ?>
-            <div class="col-md-6 mb-3">
-              <article class="letter__item c-card h-100">
+            <div class="col-md-12 mb-3">
+              <article class="card card__letter card__letter--small letter__item">
                 <div class="c-card__body letter__body">
                   <div class="letter__edition">
                     <span><span aria-label="Edição: ">#</span> <?php echo $edition; ?></span>
@@ -56,33 +68,22 @@ get_header(); ?>
               </article>
             </div>
           <?php endwhile; ?>
+          <div class="pagination d-flex justify-content-center">
+            <?php
+              echo odin_pagination( 2, 1, false, $letters); 
+            ?>
+          </div>
           <?php wp_reset_postdata(); ?>
+          
           <?php endif; ?>
         </div>
       </div>
       <div class="col-md-4">
-        <article class="newsletter">
-          <h2 class="newsletter__title">Assine a Pulis <span lang="en">Letters</span></h2>
-          <div class="newsletter__intro">
-            <p>
-              Junte-se a mais de 200 assinantes e receba dicas sobre acessibilidade
-              Obsidian e organização digital toda quinta-feira de manhã.
-            </p>
-          </div>
-          <form class="c-newsletter__form" id="form-newsletter">
-            <div class="c-newsletter-container">
-              <div class="c-newsletter__form-field">
-                <label for="email" class="visually-hidden">E-mail</label>
-                <input type="email" id="email" name="fields[email]" required pattern="[^@\s]+@[^@\s]+\.[^@\s]+" placeholder="Seu melhor e-mail" class="form-control">
-              </div>
-
-              <div class="c-newsletter__form-field">
-                <input type="submit" class="c-newsletter__form-button btn btn-primary" value="Assinar" />
-              </div>
-            </div>
-          </form>
-        </article>
+        <?php require_once( 'template-parts/components/newsletter-sidebar.php' ); ?>
       </div>
+    </div>
+    <div class="row">
+      <?php get_template_part( 'template-parts/components/home', 'testimonials' ); ?>
     </div>
   </div>
 </main><!-- #main -->
