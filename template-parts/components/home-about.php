d5de407c47a31eab90c5eb41b8c971bd3c4152e2
Bruno Pulis <brunopulis@protonmail.com>
2024-11-21 13:42:07 -0300

    refactor: remove paragraph

--- template-parts/components/home-about.php
+++ template-parts/components/home-about.php
@@ -5,9 +5,10 @@
         <img src="<?php echo TEMPLATE_PATH; ?>/assets/images/palestras.webp" loading="lazy" class="img-fluid" width="450" height="300" alt="Bruno Pulis palestrando em um evento.">
       </div>
       <div class="col-lg-6">
-        <h2 id="about-title">Sobre mim</h2>
-        <p>Referência nacional sobre acessibilidade digital, com 15 anos de experiência em desenvolvimento de software e acessibilidade.</p>
-        <p>Criador do <a href="https://github.com/brunopulis/awesome-a11y" lang="en">Awesome A11y</a> e apoiador de diversas iniciativas <em lang="en">open source</em> voltadas para <em lang="en">frontend</em> e acessibilidade.</p>
+        <h2 id="about-title">Curioso sobre mim?</h2>
+        <p>Sou um desenvolvedor especializado em acessibilidade digital, apaixonado por criar produtos digitais que seja fáceis, práticos, rápidos e tenham uma ótima experiência para os usuários.</p> 
+        <p>Comecei minha carreira em 2010 e, desde então, evoluí trabalhando com diversas tecnologias. Gosto de participar de todo o processo de desenvolvimento, desde a ideia inicial até a entrega do produto.</p>
+        <p>Sou consultor em acessibilidade em tempo integral e estou disponível para novos projetos. Se precisar de ajuda para trazer sua ideia à vida, <a href="<?php echo bloginfo( 'url' ); ?>/contato">entre em contato</a>.</p>    
         <a href="<?php echo bloginfo( 'url' ); ?>/sobre" class="button button--blue">conheça minha trajetória</a>
       </div>
     </div>
